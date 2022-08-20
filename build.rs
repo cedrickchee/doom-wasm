176d97f4fb04ba7f764461dc8c1ff360a604b1cc
Cedric Chee <cedric@invictusbyte.com>
2022-08-21 04:54:07 +0800

    add a minimal Clang compiler runtime library
    Am I compiling this correctly? Am I using the wasm32 imports?
    
    But the symbols __extendsftf2 and __extenddftf2 are now defined.

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 static DOOM_SRC: &str = "linuxdoom-1.10";
 static LIBC_SRC: &str = "musl-1.2.2";
+static LIBGCC_SRC: &str = "clang_compiler_rt";
 
 fn main() {
     // Original Doom sources
@@ -27,6 +28,18 @@ fn main() {
     }
     println!("cargo:rustc-link-search={}/lib/", LIBC_SRC);
     println!("cargo:rustc-link-lib=c");
+
+    // compiler runtime, with e.g., floating point functions
+    println!("cargo:rerun-if-changed={}", LIBGCC_SRC);
+    let status = Command::new("make")
+        .args(&["-C", LIBGCC_SRC, "build/libclang_rt.builtins-wasm32.a"])
+        .status()
+        .expect("failed to start compiler_rt make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+    println!("cargo:rustc-link-search={}/build", LIBGCC_SRC);
+    println!("cargo:rustc-link-lib=clang_rt.builtins-wasm32");
 
     // libraries which should be removed before going to wasm
     // println!("cargo:rustc-link-lib=X11");
