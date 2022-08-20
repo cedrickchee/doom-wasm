ad85592a0544d891c73e2d45d87e3974019b4d8b
Cedric Chee <cedric@invictusbyte.com>
2022-08-21 05:24:08 +0800

    musllibc wasm32 headers instead of those of my machine

--- build.rs
+++ build.rs
@@ -5,18 +5,6 @@ static LIBC_SRC: &str = "musl-1.2.2";
 static LIBGCC_SRC: &str = "clang_compiler_rt";
 
 fn main() {
-    // Original Doom sources
-    println!("cargo:rerun-if-changed={}", DOOM_SRC);
-    let status = Command::new("make")
-        .args(&["-C", DOOM_SRC, "linux/liblinuxxdoom.a"])
-        .status()
-        .expect("failed to start doom make");
-    if !status.success(){
-        panic!("Failed to make: {}", status);
-    }
-    println!("cargo:rustc-link-search={}/linux", DOOM_SRC);
-    println!("cargo:rustc-link-lib=linuxxdoom");
-
     // musl libc
     println!("cargo:rerun-if-changed={}", LIBC_SRC);
     let status = Command::new("make")
@@ -29,6 +17,15 @@ fn main() {
     println!("cargo:rustc-link-search={}/lib/", LIBC_SRC);
     println!("cargo:rustc-link-lib=c");
 
+    // system headers for wasm32
+    let status = Command::new("make")
+        .args(&["-C", LIBC_SRC, "install-headers"])
+        .status()
+        .expect("failed to start musl libc install-headers make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+
     // compiler runtime, with e.g., floating point functions
     println!("cargo:rerun-if-changed={}", LIBGCC_SRC);
     let status = Command::new("make")
@@ -40,6 +37,18 @@ fn main() {
     }
     println!("cargo:rustc-link-search={}/build", LIBGCC_SRC);
     println!("cargo:rustc-link-lib=clang_rt.builtins-wasm32");
+
+    // Original Doom sources
+    println!("cargo:rerun-if-changed={}", DOOM_SRC);
+    let status = Command::new("make")
+        .args(&["-C", DOOM_SRC, "linux/liblinuxxdoom.a"])
+        .status()
+        .expect("failed to start doom make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+    println!("cargo:rustc-link-search={}/linux", DOOM_SRC);
+    println!("cargo:rustc-link-lib=linuxxdoom");
 
     // libraries which should be removed before going to wasm
     // println!("cargo:rustc-link-lib=X11");
