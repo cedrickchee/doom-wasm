d6a497cba36efa44ce77cdfa6ce4204b4c11ff8a
Cedric Chee <cedric@invictusbyte.com>
2022-08-21 03:30:25 +0800

    statically linking a stripped down musl libc
    Some effort to make musl compile on wasm32.
    
    Completely untested!
    
    We now have MORE symbols in our wasm32 to import, but the complicated
    string functions (well, some of them) are gone.
    
    Here is the deal we made:
    
    +"__addtf3"
    -"__ctype_b_loc"
    -"__ctype_toupper_loc"
    -"__errno_location"
    +"copysignl"
    +"__divtf3"
    +"___errno_location"
    -"fclose"
    -"feof"
    -"fflush"
    +"__extenddftf2"
    +"__extendsftf2"
    +"fabsl"
    +"__fixtfsi"
    +"__fixunstfsi"
    +"__floatsitf"
    +"__floatunsitf"
    +"fmodl"
    -"fprintf"
    +"__fpclassifyl"
    -"fread"
    -"fseek"
    -"ftell"
    +"fputs"
    +"free"
    +"frexpl"
    -"fwrite"
    +"__getf2"
    -"__isoc99_fscanf"
    -"__isoc99_sscanf"
    +"__lock"
    +"__lockfile"
    +"mbrtowc"
    +"mbsinit"
    +"__multf3"
    +"__netf2"
    -"printf"
    +"__overflow"
    -"puts"
    -"setbuf"
    +"scalbn"
    +"scalbnl"
    -"sprintf"
    -"strcasecmp"
    -"strcmp"
    -"strcpy"
    -"strlen"
    -"strncasecmp"
    -"strncpy"
    +"__signbitl"
    +"__stdio_close"
    +"__stdio_seek"
    +"__stdio_write"
    +"__stdout_write"
    +"strerror"
    +"__subtf3"
    +"__toread"
    +"__towrite"
    +"__trunctfdf2"
    +"__trunctfsf2"
    +"__uflow"
    +"__unlock"
    +"__unlockfile"
    -"vfprintf"
    +"wctomb"

--- build.rs
+++ build.rs
@@ -1,31 +1,32 @@
 use std::process::Command;
 
 static DOOM_SRC: &str = "linuxdoom-1.10";
+static LIBC_SRC: &str = "musl-1.2.2";
 
 fn main() {
+    // Original Doom sources
     println!("cargo:rerun-if-changed={}", DOOM_SRC);
-
-    // Compile to executable
-    // If compile failed, don't panic, instead continue to compile the static lib.
-    let build_status = Command::new("make")
-        .args(&["-C", DOOM_SRC])
-        .status()
-        .expect("failed to start make");
-    if !build_status.success(){
-        println!("failed to create executable: {build_status}");
-    }
-
-    // Compile to static library
-    let ar_status = Command::new("make")
+    let status = Command::new("make")
         .args(&["-C", DOOM_SRC, "linux/liblinuxxdoom.a"])
         .status()
-        .expect("failed to start make");
-    if !ar_status.success(){
-        panic!("make failed to compile to static lib: {}", ar_status);
+        .expect("failed to start doom make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
     }
-
     println!("cargo:rustc-link-search={}/linux", DOOM_SRC);
     println!("cargo:rustc-link-lib=linuxxdoom");
+
+    // musl libc
+    println!("cargo:rerun-if-changed={}", LIBC_SRC);
+    let status = Command::new("make")
+        .args(&["-C", LIBC_SRC, "lib/libc.a"])
+        .status()
+        .expect("failed to start musl libc make");
+    if !status.success(){
+        panic!("Failed to make: {}", status);
+    }
+    println!("cargo:rustc-link-search={}/lib/", LIBC_SRC);
+    println!("cargo:rustc-link-lib=c");
 
     // libraries which should be removed before going to wasm
     // println!("cargo:rustc-link-lib=X11");
