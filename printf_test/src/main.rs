7e1304fe92f773c8381d019fc0678c4b4e325a87
Cedric Chee <cedric@invictusbyte.com>
2022-08-23 15:52:57 +0800

    well, using the right type signature resolved the wrong type error -_-

--- printf_test/src/main.rs
+++ printf_test/src/main.rs
@@ -55,7 +55,8 @@ extern "C" fn  __syscall3(n: i32, a1: i32, a2: i32, a3: i32) -> i32 {
 }
 
 #[no_mangle]
-extern "C" fn __stdout_write() {
+// extern "C" fn __stdout_write() {
+extern "C" fn __stdout_write(_: i32, _: i32, _: i32) -> i32 {
     panic!("__stdout_write unimplemented");
 }
 
