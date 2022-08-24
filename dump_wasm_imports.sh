f15df01cd6cae11248ed5a0bcc44febcba3f5f2e
Cedric Chee <cedric@invictusbyte.com>
2022-08-24 14:06:16 +0800

    refactor, split the huge `main.rs` into a library
    I learned about `mod` (inline a file) vs. `use` (reference the public
    stuff in a library).
    
    Also getting the `log!` macro right was a struggle. `$crate::` vs
    `crate::` was really confusing for me as a beginner.

--- dump_wasm_imports.sh
+++ dump_wasm_imports.sh
@@ -3,4 +3,4 @@
 wasm2wat=/home/neo/dev/scratch/github/WebAssembly/wabt/build/wasm2wat
 
 # apt install wabt
-$wasm2wat "${1}" | grep '\(import\|export\)'
+$wasm2wat "${1}" | grep '\(import \|export \)'
